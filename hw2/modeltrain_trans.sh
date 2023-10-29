7736e9c350421db88450a867e8981d6c97f7e51c
= <=>
2023-10-29 17:37:02 -0500

    add job file for transformed image classification

---
+++ hw2/modeltrain_trans.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # 8 processor core(s) per node 
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=gpu    # gpu node(s)
+#SBATCH --job-name="dl2classa"
+#SBATCH --mail-user=sjx@iastate.edu   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load r
+export R_LIBS_USER=/work/classtmp/carrice/Rlibs
+mkdir -p $R_LIBS_USER
+Rscript train2classtrans.R
